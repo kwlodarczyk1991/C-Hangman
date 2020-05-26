bfd7b11cda1a199ce283a5cade851dfadd7ff2a0
kwlodarczyk1991 <krzoku91@gmail.com>
2020-05-26 22:24:21 +0200

    Random List

---
+++ SecretPassword.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hangman1
+{
+    public class SecretPassword
+    {
+        public static void Main(String[] args)
+        {
+            List<string> capitol = new List<string>();
+
+            capitol.Add("Prague");
+            capitol.Add("Rome");
+            capitol.Add("Paris");
+            capitol.Add("Berlin");
+            capitol.Add("Warsaw");
+            capitol.Add("Moscow");
+            capitol.Add("Madrid");
+            capitol.Add("Lisbon");
+            capitol.Add("Vienna");
+            capitol.Add("Zagreb");
+
+            Random randcap = new Random();
+            int index = randcap.Next(capitol.Count);
+            string randomString = capitol[index];
+        }
+         
+
+        
+      
+    }
+}
